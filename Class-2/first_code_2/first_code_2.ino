a08aa382986cd2990a449396743778a7f48b9722
Dr. Bleep <drbleep@bleeplabs.com>
2021-02-08 20:02:19 -0600

    Class 2

---
+++ Class-2/first_code_2/first_code_2.ino
@@ -0,0 +1,53 @@
+//bleep base basic controls
+
+//Include means add code from another file. This allows use more functions
+#include "bleep_base.h" //must be included at the top
+
+//then you can declare any variables you want.
+unsigned long current_time;
+unsigned long prev_time[8]; //an array of 8 variables all named "prev_time"
+float lfo1 = 1; //float can contain decimals
+float lfo2 = 1;
+int rate1;
+int rate2;
+int rate3 = 33;
+float h1;
+float h2;
+float b1;
+float b2;
+float led_state;
+
+void setup() {
+  start_bleep_base(); //run this first in setup
+}
+
+void loop() {
+  update_controls(); //run this first in the loop
+  current_time = millis();
+
+  rate1 = (1.0 - potRead(2)) * 1000.0;
+
+  if (current_time - prev_time[1] > rate1) {
+    prev_time[1] = current_time;
+
+    if (led_state == 0) {
+      led_state = 1;
+    }
+    else {
+      led_state = 0;
+    }
+
+  }
+
+
+  if (current_time - prev_time[0] > 30) {
+    prev_time[0] = current_time;
+    h1 = potRead(0);
+    b1 = potRead(1) * led_state;
+    set_LED(0, h1, 1.0, b1);
+    set_LED(1, 0, 0, 0);
+    LEDs.show();
+  }
+
+
+}// loop is over
