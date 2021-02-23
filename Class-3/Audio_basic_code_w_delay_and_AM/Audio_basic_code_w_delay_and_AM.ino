c37e76aa6a45654322e54e135d6d8829d52d9eca
Dr. Bleep <drbleep@bleeplabs.com>
2021-02-22 20:45:52 -0600

    class 3

---
+++ Class-3/Audio_basic_code_w_delay_and_AM/Audio_basic_code_w_delay_and_AM.ino
@@ -0,0 +1,192 @@
+// The block we copied from the tool is pasted below
+// design tool: https://www.pjrc.com/teensy/gui/
+
+// "#include" means add another file to our code
+// So far we've been copy and pasting things in but we can just tell our code to look
+// in a library for more functions and data
+// These are all necessary to get audio working but we don't need to do anything besides include them
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioSynthWaveform       waveform3;      //xy=113.00566482543945,186.00564193725586
+AudioSynthWaveform       waveform4;      //xy=130.0056610107422,244.0056610107422
+AudioSynthWaveform       waveform2;      //xy=130.9999713897705,137.99996280670166
+AudioSynthWaveform       waveform5;      //xy=133.75,333.75
+AudioSynthWaveform       waveform1;      //xy=153.99996948242188,91.99997901916504
+AudioMixer4              mixer1;         //xy=286.49996185302734,170.99995613098145
+AudioEffectMultiply      multiply1;      //xy=438.750036239624,203.7500171661377
+AudioEffectDelay         delay1;         //xy=459.75562286376953,444.0056266784668
+AudioMixer4              mixer2;         //xy=470.7553825378418,313.50562286376953
+AudioMixer4              mixer3;         //xy=637.0056304931641,153.75568008422852
+AudioOutputI2S           i2s1;           //xy=788.7498474121094,90.99997282028198
+AudioConnection          patchCord1(waveform3, 0, mixer1, 2);
+AudioConnection          patchCord2(waveform4, 0, mixer1, 3);
+AudioConnection          patchCord3(waveform2, 0, mixer1, 1);
+AudioConnection          patchCord4(waveform5, 0, multiply1, 1);
+AudioConnection          patchCord5(waveform1, 0, mixer1, 0);
+AudioConnection          patchCord6(mixer1, 0, multiply1, 0);
+AudioConnection          patchCord7(multiply1, 0, mixer3, 0);
+AudioConnection          patchCord8(multiply1, 0, mixer2, 0);
+AudioConnection          patchCord9(delay1, 0, mixer2, 1);
+AudioConnection          patchCord10(delay1, 1, mixer2, 2);
+AudioConnection          patchCord11(delay1, 2, mixer2, 3);
+AudioConnection          patchCord12(mixer2, delay1);
+AudioConnection          patchCord13(mixer2, 0, mixer3, 1);
+AudioConnection          patchCord14(mixer3, 0, i2s1, 0);
+AudioConnection          patchCord15(mixer3, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=244.49993133544922,642.0000157356262
+// GUItool: end automatically generated code
+
+
+
+
+#include "bleep_base.h" //Then we can add this line that we will still need
+
+//then you can declare any variables you want.
+unsigned long current_time;
+unsigned long prev_time[8]; //an array of 8 variables all named "prev_time"
+float freq1;
+float freq2;
+float freq3;
+float freq4;
+float freq5;
+float amp1, amp2, amp3, amp4;
+float dtime;
+
+void setup() {
+  start_bleep_base(); //run this first in setup
+
+  //there's a lot we need to do in setup now but most of it is just copy paste.
+  // This first group should only be done in setup how much RAM to set aside for the audio library to use.
+  // The audio library uses blocks of a set size so this is not a percentage or kilobytes, just a kind of arbitrary number.
+  // On our Teensy 4.0 we can go up to almost 2000 but that won't leave any RAM for anyone else.
+  // It's usually the delay and reverb that hog it.
+  AudioMemory(1000);
+
+  sgtl5000_1.enable(); //Turn the adapter board on
+  sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN); //Tell it what input we want to use. Not necessary is you're not using the ins
+  sgtl5000_1.lineInLevel(10); //The volume of the input. 0-15 with 15 bing more amplifications
+  //sgtl5000_1.inputSelect(AUDIO_INPUT_MIC);
+  //sgtl5000_1.micGain(13); //0 - 63bd of gain.
+
+  //headphone jack output volume. Goes from 0.0 to 1.0 but a 100% signal will clip over .8 or so.
+  // For headphones it's pretty loud at .4
+  // There are lots of places we can change the final volume level.
+  // For now lets set this one once and leave it alone.
+  sgtl5000_1.volume(0.25);
+
+  //The line out has a seperate level control but it's not meant to be adjusted like the volume function above.
+  // If you're not using the line out don't worry about it.
+  sgtl5000_1.lineOutLevel(21); //11-32, the smaller the louder. 21 is about 2 Volts peak to peak
+
+
+  //This next group can be done anywhere in the code but we want to start things with these
+  // values and change some of them in the loop.
+
+  //Notice we start by writing the object we want, then a period, then the function
+  // begin(volume from 0.0-1.0 , frequency , shape of oscillator)
+  // See the tool for more info https://www.pjrc.com/teensy/gui/?info=AudioSynthWaveform
+  waveform1.begin(1, 220.0, WAVEFORM_SINE);
+  waveform2.begin(1, 440.0, WAVEFORM_SINE);
+  waveform3.begin(1, 220.0, WAVEFORM_SINE);
+  waveform4.begin(1, 440.0, WAVEFORM_SINE);
+  waveform5.begin(1, 440.0, WAVEFORM_SINE);
+
+  //The mixer has four inputs we can change the volume of
+  // gain.(channel from 0 to 3, gain from 0.0 to a large number)
+  // A gain of 1 means the output is the same as the input.
+  // .5 would be half volume and 2 would be double
+  // Since we have two oscillators coming in that are already "1" We should take them down by half so we don't clip.
+  // If you go over "1" The top or bottom of the wave is just slammed against a wall
+  mixer1.gain(0, .25);
+  mixer1.gain(1, .25);
+  mixer1.gain(2, .25);
+  mixer1.gain(3, .25);
+
+  mixer2.gain(0, .5);
+  mixer2.gain(1, .5);
+
+  mixer3.gain(0, .5);
+  mixer3.gain(1, .5);
+
+  delay1.delay(0, 1000);
+  delay1.delay(0, 900);
+  delay1.delay(0, 800);
+  //delay2.delay(0, 50);
+
+} //setup is over
+
+
+void loop() {
+  update_controls();
+  current_time = millis();
+
+  freq1 = (1.0 - potRead(0)) * 1000.0;
+  //freq1=potRead(0)*1000.0;
+  waveform1.frequency(freq1);
+
+  freq2 = freq1 * (potRead(1) + 1.0);
+  waveform2.frequency(freq2);
+
+  freq3 = freq1 * (potRead(2) + 1.0);
+  waveform3.frequency(freq3);
+
+  freq4 = freq1 * (potRead(3) + 1.0);
+  waveform4.frequency(freq4);
+
+  freq5 = potRead(7)*400.0;
+  waveform5.frequency(freq5);
+
+  
+  amp1 = 1.0 - potRead(4);
+  mixer2.gain(1, amp1);
+  mixer2.gain(2, amp1);
+  mixer2.gain(3, amp1);
+
+  amp2 = potRead(5);
+  amp3 = 1.0 - amp2;
+
+  mixer3.gain(0, amp2);
+  mixer3.gain(1, amp3);
+
+  dtime = potRead(6) * 1000.0; 
+  // delay1.delay(0, dtime);  //sounds glitchy
+
+
+  /*
+    amp1 = potRead(4) * .25;
+    mixer1.gain(0, amp1);
+
+    amp2 = potRead(5) * .25;
+    mixer1.gain(1, amp2);
+
+    amp3 = potRead(6) * .25;
+    mixer1.gain(2, amp3);
+
+    amp4 = potRead(7) * .25;
+    mixer1.gain(3, amp4);
+  */
+
+  //We don't have to do anything in the loop since the audio library will jut keep doing what we told it in the setup
+  if (current_time - prev_time[0] > 100) {
+    prev_time[0] = current_time;
+    Serial.println(amp2);
+    Serial.println(amp3);
+
+    //Here we print out the usage of the audio library
+    // If we go over 90% processor usage or get near the value of memory blocks we set aside in the setup we'll have issues or crash.
+    // If you're using too many block, jut increas the number up top untill you're over it by a few
+    Serial.print("processor: ");
+    Serial.print(AudioProcessorUsageMax());
+    Serial.print("%    Memory: ");
+    Serial.print(AudioMemoryUsageMax());
+    Serial.println();
+    AudioProcessorUsageMaxReset(); //We need to reset these values so we get a real idea of what the audio code is doing rather than just peaking in every half a second
+    AudioMemoryUsageMaxReset();
+  }
+
+}// loop is over
---
+++ Class-3/Audio_basic_code_w_delay_and_AM/bleep_base.h
@@ -0,0 +1,331 @@
+// all the functinons I made to check the controls and wok the LEDs live here
+// We'll talk about whats they are really doing on later in the course
+float max_brightness = .1; //change this to increase the max brightness of the LEDs. 1.0 is very bright
+
+#include <MIDI.h>
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
+
+#include <WS2812Serial.h>
+#define num_of_leds 2
+#define led_data_pin 14 // only these pins can be used on the Teensy 3.2:  1, 5, 8, 10, 31
+byte drawingMemory[num_of_leds * 3];       //  3 bytes per LED
+DMAMEM byte displayMemory[num_of_leds * 12]; // 12 bytes per LED
+WS2812Serial LEDs(num_of_leds, displayMemory, drawingMemory, led_data_pin, WS2812_GRB);
+
+#include <ADC.h>
+#include <ADC_util.h>
+ADC *adc = new ADC();
+
+uint32_t bounce_time[8];
+byte bounce_latch[8];
+
+byte mux_select, prev_mux_select;
+int raw_pot_reading[8];
+int sm_pot_reading[8];
+float pot_reading[8];
+int button_reading[8];
+int prev_button_reading[8];
+int button_state[8];
+uint32_t pm[8];
+int pcellRaw, pcellSmooth;
+
+#define ctrl_pin1 2
+#define ctrl_pin2 3
+#define ctrl_pin3 4
+
+#define digital_read_pin 5
+#define analog_pin1 A1
+
+#define FELL 4
+#define ROSE 5
+#define IS_PRESSED 0
+#define NOT_PRESSED 1
+
+#define  bounce_lockout_length 10
+byte first_time = 1;
+
+void start_bleep_base() {
+
+  LEDs.begin(); //must be done in setup for the LEDs to work.
+  LEDs.setPixelColor(0, 0, 0, 0);
+  LEDs.setPixelColor(1, 0, 0, 0);
+  LEDs.show(); // after we've set what we want all the LEDs to be we send the data out through this function
+
+  MIDI.turnThruOff();
+  MIDI.begin(1);
+  Serial.begin(57600);
+
+  pinMode(analog_pin1, INPUT);
+
+  pinMode(digital_read_pin, INPUT_PULLUP);
+  pinMode(ctrl_pin1, OUTPUT);
+  pinMode(ctrl_pin2, OUTPUT);
+  pinMode(ctrl_pin3, OUTPUT);
+
+  adc->adc0->setAveraging(8);
+  adc->adc0->setResolution(12); // teensy 4 is 10b 0-1023
+  // it can be any of the ADC_MED_SPEED enum: VERY_LOW_SPEED, LOW_SPEED, MED_SPEED, HIGH_SPEED or VERY_HIGH_SPEED
+
+  adc->adc0->setConversionSpeed(ADC_CONVERSION_SPEED::LOW_SPEED); // change the conversion speed
+  adc->adc0->setSamplingSpeed(ADC_SAMPLING_SPEED::LOW_SPEED); // change the sampling speed
+
+  adc->adc1->setAveraging(8);
+  adc->adc1->setResolution(12); // teensy 4 is 10b 0-1023
+  // it can be any of the ADC_MED_SPEED enum: VERY_LOW_SPEED, LOW_SPEED, MED_SPEED, HIGH_SPEED or VERY_HIGH_SPEED
+
+  adc->adc1->setConversionSpeed(ADC_CONVERSION_SPEED::MED_SPEED); // change the conversion speed
+  adc->adc1->setSamplingSpeed(ADC_SAMPLING_SPEED::MED_SPEED); // change the sampling speed
+}
+
+
+//Smooth
+// based on https://playground.arduino.cc/Main/DigitalSmooth/
+// This function continuously samples an input and puts it in an array that is "samples" in length.
+// This array has a new "raw_in" value added to it each time "smooth" is called and an old value is removed
+// It throws out the top and bottom 15% of readings and averages the rest
+
+#define maxarrays 9 //max number of different variables to smooth
+#define maxsamples 13 //max number of points to sample and 
+//reduce these numbers to save RAM
+
+unsigned int smoothArray[maxarrays][maxsamples];
+
+// sel should be a unique number for each occurrence
+// samples should be an odd number greater that 7. It's the length of the array. The larger the more smooth but less responsive
+// raw_in is the input. positive numbers in and out only.
+
+unsigned int smooth(byte sel, unsigned int samples, unsigned int raw_in) {
+  int j, k, temp, top, bottom;
+  long total;
+  static int i[maxarrays];
+  static int sorted[maxarrays][maxsamples];
+  boolean done;
+
+  i[sel] = (i[sel] + 1) % samples;    // increment counter and roll over if necessary. -  % (modulo operator) rolls over variable
+  smoothArray[sel][i[sel]] = raw_in;                 // input new data into the oldest slot
+
+  for (j = 0; j < samples; j++) { // transfer data array into anther array for sorting and averaging
+    sorted[sel][j] = smoothArray[sel][j];
+  }
+
+  done = 0;                // flag to know when we're done sorting
+  while (done != 1) {      // simple swap sort, sorts numbers from lowest to highest
+    done = 1;
+    for (j = 0; j < (samples - 1); j++) {
+      if (sorted[sel][j] > sorted[sel][j + 1]) {    // numbers are out of order - swap
+        temp = sorted[sel][j + 1];
+        sorted[sel] [j + 1] =  sorted[sel][j] ;
+        sorted[sel] [j] = temp;
+        done = 0;
+      }
+    }
+  }
+
+  //I cahnged this to jsut 2 off the top and bottom
+  bottom = 2;
+  top = samples - 2;
+  k = 0;
+  total = 0;
+  for ( j = bottom; j < top; j++) {
+    total += sorted[sel][j];  // total remaining indices
+    k++;
+  }
+  return total / k;    // divide by number of samples
+}
+
+float potRead(byte sel) {
+  if (sel > 7) {
+    sel = 7;
+  }
+  return pot_reading[sel];
+}
+
+float potReadRaw(byte sel) {
+  if (sel > 7) {
+    sel = 7;
+  }
+  return raw_pot_reading[sel];
+}
+float potReadSmooth(byte sel) {
+  if (sel > 7) {
+    sel = 7;
+  }
+  return sm_pot_reading[sel];
+}
+float pcellRead() {
+  return pcellSmooth / 4095.0;
+}
+
+int buttonState(byte num) {
+  return button_state[num];
+}
+int buttonRead(byte num) {
+  return button_reading[num];
+}
+
+void update_controls() {
+  uint32_t cm = millis();
+
+  if (button_reading[mux_select] == 0 ) {
+    button_state[mux_select] = IS_PRESSED;
+  }
+  if (button_reading[mux_select] == 1 ) {
+    button_state[mux_select] = NOT_PRESSED;
+  }
+
+  if (prev_button_reading[mux_select] != button_reading[mux_select]) {
+
+    if (bounce_latch[mux_select] == 0) {
+      bounce_time[mux_select] = millis();
+      if (prev_button_reading[mux_select] == 1 && button_reading[mux_select] == 0) {
+        button_state[mux_select] = FELL;
+      }
+      if (prev_button_reading[mux_select] == 0 && button_reading[mux_select] == 1) {
+        button_state[mux_select] = ROSE;
+      }
+    }
+    bounce_latch[mux_select] = 1;
+  }
+
+  if (bounce_latch[mux_select] == 1 && millis() - bounce_time[mux_select] > bounce_lockout_length) {
+    bounce_time[mux_select] = 0;
+    bounce_latch[mux_select] = 0;
+    // Serial.print(mux_select);      Serial.println("rst");
+  }
+  float high = 4084.0; //12 bits should be 4095 but withg the resistance of the mux it doens't quite get there
+  if (cm - pm[2] > 3) {
+    pm[2] = cm;
+    pcellRaw = adc->adc1->analogRead(A2);
+    pcellSmooth = smooth(8, 7, pcellRaw);
+    for (int m = 0; m < 8; m++) {
+      sm_pot_reading[m] = smooth(m, 11, raw_pot_reading[m]);
+      if (sm_pot_reading[m] > high) {
+        sm_pot_reading[m] = high;
+      }
+      pot_reading[m] = sm_pot_reading[m] / high;
+    }
+    pinMode(analog_pin1, INPUT);
+    delayMicroseconds(2000);
+    raw_pot_reading[mux_select] = high - adc->adc0->analogRead(analog_pin1);
+    pinMode(analog_pin1, OUTPUT);
+    
+    if (first_time == 1) {
+      button_reading[mux_select] = 1;
+      prev_button_reading[mux_select] = 1;
+    }
+    else {
+      prev_button_reading[mux_select] = button_reading[mux_select];
+      button_reading[mux_select] = digitalRead(digital_read_pin);
+    }
+
+    mux_select++;
+    if (mux_select > 7) {
+      mux_select = 0;
+      first_time = 0;
+    }
+    digitalWrite(ctrl_pin1, bitRead(mux_select, 0));
+    digitalWrite(ctrl_pin2, bitRead(mux_select, 1));
+    digitalWrite(ctrl_pin3, bitRead(mux_select, 2));
+
+  }
+}
+
+void MIDI_read () {
+  int type, note, velocity, channel, d1, d2;
+  if (MIDI.read()) {                    // Is there a MIDI message incoming ?
+    byte type = MIDI.getType();
+    switch (type) {
+      case midi::NoteOn:
+        note = MIDI.getData1();
+        velocity = MIDI.getData2();
+        channel = MIDI.getChannel();
+        if (velocity > 0) {
+          Serial.println(String("Note On:  ch=") + channel + ", note=" + note + ", velocity=" + velocity);
+        } else {
+          Serial.println(String("Note Off: ch=") + channel + ", note=" + note);
+        }
+        break;
+      case midi::NoteOff:
+        note = MIDI.getData1();
+        velocity = MIDI.getData2();
+        channel = MIDI.getChannel();
+        //Serial.println(String("Note Off: ch=") + channel + ", note=" + note + ", velocity=" + velocity);
+        break;
+      default:
+        d1 = MIDI.getData1();
+        d2 = MIDI.getData2();
+        //  Serial.println(String("Message, type=") + type + ", data = " + d1 + " " + d2);
+    }
+  }
+
+}
+
+
+
+//This function is a little different than you might see in other libraries but it works pretty similar
+// instead of 0-255 you see in other libraries this is all 0-1.0
+// you can copy this to the bottom of any code as long as the declarations at the top in "led biz" are done
+
+//set_pixel_HSV(led to change, hue,saturation,value aka brightness)
+// led to change is 0-63
+// all other are 0.0 to 1.0
+// hue - 0 is red, then through the ROYGBIV to 1.0 as red again
+// saturation - 0 is fully white, 1 is fully colored.
+// value - 0 is off, 1 is the value set by max_brightness
+// (it's not called brightness since, unlike in photoshop, we're going from black to fully lit up
+
+//based on https://stackoverflow.com/questions/3018313/algorithm-to-convert-rgb-to-hsv-and-hsv-to-rgb-in-range-0-255-for-both
+
+void set_LED(int pixel, float fh, float fs, float fv) {
+  byte RedLight;
+  byte GreenLight;
+  byte BlueLight;
+
+  byte h = fh * 255;
+  byte s = fs * 255;
+  byte v = fv * max_brightness * 255;
+
+  h = (h * 192) / 256;  // 0..191
+  unsigned int i = h / 32;   // We want a value of 0 thru 5
+  unsigned int f = (h % 32) * 8;   // 'fractional' part of 'i' 0..248 in jumps
+
+  unsigned int sInv = 255 - s;  // 0 -> 0xff, 0xff -> 0
+  unsigned int fInv = 255 - f;  // 0 -> 0xff, 0xff -> 0
+  byte pv = v * sInv / 256;  // pv will be in range 0 - 255
+  byte qv = v * (256 - s * f / 256) / 256;
+  byte tv = v * (256 - s * fInv / 256) / 256;
+
+  switch (i) {
+    case 0:
+      RedLight = v;
+      GreenLight = tv;
+      BlueLight = pv;
+      break;
+    case 1:
+      RedLight = qv;
+      GreenLight = v;
+      BlueLight = pv;
+      break;
+    case 2:
+      RedLight = pv;
+      GreenLight = v;
+      BlueLight = tv;
+      break;
+    case 3:
+      RedLight = pv;
+      GreenLight = qv;
+      BlueLight = v;
+      break;
+    case 4:
+      RedLight = tv;
+      GreenLight = pv;
+      BlueLight = v;
+      break;
+    case 5:
+      RedLight = v;
+      GreenLight = pv;
+      BlueLight = qv;
+      break;
+  }
+  LEDs.setPixelColor(pixel, RedLight, GreenLight, BlueLight);
+}
