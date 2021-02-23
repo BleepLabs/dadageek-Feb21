fed28eb9fb2e088b8fe1293d41194f5e7e15be96
Dr. Bleep <drbleep@bleeplabs.com>
2021-02-22 19:58:15 -0600

    class 3

---
+++ Class-3/Audio_basic_code_w_control_2/Audio_basic_code_w_control_2.ino
@@ -0,0 +1,136 @@
+// The block we copied from the tool is pasted below
+// design tool: https://www.pjrc.com/teensy/gui/
+
+// "#include" means add another file to our code
+// So far we've been copy and pasting things in but we can just tell our code to look
+// in a library for more functions and data
+// These are all necessary to get audio working but we don't need to do anything besides include them
+
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioSynthWaveform       waveform3;      //xy=149.00569534301758,227.00566482543945
+AudioSynthWaveform       waveform4;      //xy=166.0056915283203,285.0056838989258
+AudioSynthWaveform       waveform2;      //xy=167.00000190734863,178.99998569488525
+AudioSynthWaveform       waveform1;      //xy=190,133.00000190734863
+AudioMixer4              mixer1;         //xy=417.9999771118164,188.99998474121094
+AudioOutputI2S           i2s1;           //xy=620.9999599456787,188.99998474121094
+AudioConnection          patchCord1(waveform3, 0, mixer1, 2);
+AudioConnection          patchCord2(waveform4, 0, mixer1, 3);
+AudioConnection          patchCord3(waveform2, 0, mixer1, 1);
+AudioConnection          patchCord4(waveform1, 0, mixer1, 0);
+AudioConnection          patchCord5(mixer1, 0, i2s1, 0);
+AudioConnection          patchCord6(mixer1, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=496,293
+// GUItool: end automatically generated code
+
+
+#include "bleep_base.h" //Then we can add this line that we will still need
+
+//then you can declare any variables you want.
+unsigned long current_time;
+unsigned long prev_time[8]; //an array of 8 variables all named "prev_time"
+float freq1;
+float freq2;
+float freq3;
+float freq4;
+float amp1,amp2;
+
+void setup() {
+  start_bleep_base(); //run this first in setup
+
+  //there's a lot we need to do in setup now but most of it is just copy paste.
+  // This first group should only be done in setup how much RAM to set aside for the audio library to use.
+  // The audio library uses blocks of a set size so this is not a percentage or kilobytes, just a kind of arbitrary number.
+  // On our Teensy 4.0 we can go up to almost 2000 but that won't leave any RAM for anyone else.
+  // It's usually the delay and reverb that hog it.
+  AudioMemory(100);
+
+  sgtl5000_1.enable(); //Turn the adapter board on
+  sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN); //Tell it what input we want to use. Not necessary is you're not using the ins
+  sgtl5000_1.lineInLevel(10); //The volume of the input. 0-15 with 15 bing more amplifications
+  //sgtl5000_1.inputSelect(AUDIO_INPUT_MIC);
+  //sgtl5000_1.micGain(13); //0 - 63bd of gain.
+
+  //headphone jack output volume. Goes from 0.0 to 1.0 but a 100% signal will clip over .8 or so.
+  // For headphones it's pretty loud at .4
+  // There are lots of places we can change the final volume level. 
+  // For now lets set this one once and leave it alone.
+  sgtl5000_1.volume(0.25);
+
+  //The line out has a seperate level control but it's not meant to be adjusted like the volume function above.
+  // If you're not using the line out don't worry about it. 
+  sgtl5000_1.lineOutLevel(21); //11-32, the smaller the louder. 21 is about 2 Volts peak to peak
+
+
+  //This next group can be done anywhere in the code but we want to start things with these
+  // values and change some of them in the loop.
+
+  //Notice we start by writing the object we want, then a period, then the function
+  // begin(volume from 0.0-1.0 , frequency , shape of oscillator)
+  // See the tool for more info https://www.pjrc.com/teensy/gui/?info=AudioSynthWaveform
+  waveform1.begin(1, 220.0, WAVEFORM_SINE);
+  waveform2.begin(1, 440.0, WAVEFORM_SINE);
+  waveform3.begin(1, 220.0, WAVEFORM_SINE);
+  waveform4.begin(1, 440.0, WAVEFORM_SINE);
+
+  //The mixer has four inputs we can change the volume of
+  // gain.(channel from 0 to 3, gain from 0.0 to a large number)
+  // A gain of 1 means the output is the same as the input.
+  // .5 would be half volume and 2 would be double
+  // Since we have two oscillators coming in that are already "1" We should take them down by half so we don't clip.
+  // If you go over "1" The top or bottom of the wave is just slammed against a wall
+  mixer1.gain(0, .25);
+  mixer1.gain(1, .25);
+  mixer1.gain(2, .25);
+  mixer1.gain(3, .25);
+
+} //setup is over
+
+
+void loop() {
+  update_controls();
+  current_time = millis();
+
+  freq1=(1.0-potRead(0))*1000.0;
+  //freq1=potRead(0)*1000.0;
+  waveform1.frequency(freq1);
+
+  freq2=freq1*(potRead(1)+1.0);
+  waveform2.frequency(freq2);
+
+  freq3=freq1*(potRead(2)+1.0);
+  waveform3.frequency(freq3);
+
+  freq4=freq1*(potRead(3)+1.0);
+  waveform4.frequency(freq4);
+
+/*
+  amp1=potRead(2)*.5;
+  mixer1.gain(0,amp1);
+  
+  amp2=potRead(3)*.5;
+  mixer1.gain(1,amp2);
+*/
+
+  //We don't have to do anything in the loop since the audio library will jut keep doing what we told it in the setup
+  if (current_time - prev_time[0] > 500) {
+    prev_time[0] = current_time;
+
+    //Here we print out the usage of the audio library
+    // If we go over 90% processor usage or get near the value of memory blocks we set aside in the setup we'll have issues or crash.
+    // If you're using too many block, jut increas the number up top untill you're over it by a few
+    Serial.print("processor: ");
+    Serial.print(AudioProcessorUsageMax());
+    Serial.print("%    Memory: ");
+    Serial.print(AudioMemoryUsageMax());
+    Serial.println();
+    AudioProcessorUsageMaxReset(); //We need to reset these values so we get a real idea of what the audio code is doing rather than just peaking in every half a second
+    AudioMemoryUsageMaxReset();
+  }
+
+}// loop is over
